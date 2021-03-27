f9029d01176217c0f6d9cbec0d1c1cef933ae727
Jolan Aklin <jolanaklin@outlook.com>
2021-03-27 21:03:01 +0100

    Change the credit in the main menu and change an error in files header

--- Assets/Terrain/Scripts/Ball.cs
+++ Assets/Terrain/Scripts/Ball.cs
@@ -6,7 +6,7 @@
 //it under the terms of the GNU General Public License as published by
 //the Free Software Foundation, version 3 of the License.
 
-//FileTeleporter is distributed in the hope that it will be useful,
+//Prog the robot is distributed in the hope that it will be useful,
 //but WITHOUT ANY WARRANTY; without even the implied warranty of
 //MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
 //GNU General Public License for more details.
