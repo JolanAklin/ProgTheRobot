bac0639f6d042d1f5d96cf88ff164cc9c4642e02
Jolan Aklin <jolanaklin@outlook.com>
2021-05-17 17:51:59 +0200

    Ajout des en têtes de copyright et GPLv3

--- Assets/GUI/PopUp/PopUpColor.cs
+++ Assets/GUI/PopUp/PopUpColor.cs
@@ -1,12 +1,12 @@
 // Copyright 2021 Jolan Aklin
 
-//This file is part of Prog the robot.
+//This file is part of Prog The Robot.
 
-//Prog the robot is free software: you can redistribute it and/or modify
+//Prog The Robot is free software: you can redistribute it and/or modify
 //it under the terms of the GNU General Public License as published by
 //the Free Software Foundation, version 3 of the License.
 
-//Prog the robot is distributed in the hope that it will be useful,
+//Prog The Robot is distributed in the hope that it will be useful,
 //but WITHOUT ANY WARRANTY; without even the implied warranty of
 //MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
 //GNU General Public License for more details.
--- Assets/Scripts/ScriptCloner.cs
+++ Assets/Scripts/ScriptCloner.cs
@@ -1,3 +1,19 @@
+// Copyright 2021 Jolan Aklin
+
+//This file is part of Prog The Robot.
+
+//Prog The Robot is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, version 3 of the License.
+
+//Prog The Robot is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with Prog the robot.  If not, see<https://www.gnu.org/licenses/>.
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
